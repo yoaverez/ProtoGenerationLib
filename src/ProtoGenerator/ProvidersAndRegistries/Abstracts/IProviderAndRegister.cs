a8454c833eb2696cceb6e3ca6a491521aca7748a
yoaverez <yoav.erez.01@gmail.com>
2025-04-25 13:53:04 +0300

    Implemented the IProviderAndRegister

---
+++ src/ProtoGenerator/ProvidersAndRegistries/Abstracts/IProviderAndRegister.cs
@@ -0,0 +1,13 @@
+﻿using ProtoGenerator.ProvidersAndRegistries.Abstracts.Providers;
+using ProtoGenerator.ProvidersAndRegistries.Abstracts.Registries;
+
+namespace ProtoGenerator.ProvidersAndRegistries.Abstracts
+{
+    /// <summary>
+    /// A provider and register of all the customizations.
+    /// </summary>
+    public interface IProviderAndRegister : IProvider, IRegistry
+    {
+        // Noting to do.
+    }
+}
