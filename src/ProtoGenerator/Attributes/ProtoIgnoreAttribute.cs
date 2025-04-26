e51a34333d9251442f5556b2204b9c5f5ea10550
yoaverez <yoav.erez.01@gmail.com>
2025-04-26 19:59:17 +0300

    Implemented all the attributes

---
+++ src/ProtoGenerator/Attributes/ProtoIgnoreAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProtoGenerator.Attributes
+{
+    /// <summary>
+    /// Attribute to mark fields and properties
+    /// as members that needs to be ignored.
+    /// </summary>
+    /// <remarks>
+    /// If this attribute target a property, the backfield that csharp generates automatically,
+    /// will also be ignored.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class ProtoIgnoreAttribute : Attribute
+    {
+    }
+}
---
+++ src/ProtoGenerator/Attributes/ProtoRpcAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProtoGenerator.Attributes
+{
+    /// <summary>
+    /// Attribute for marking methods as a proto service methods.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ProtoRpcAttribute : Attribute
+    {
+        /// <summary>
+        /// Represents the type of the RPC.
+        /// </summary>
+        public ProtoRpcType RpcType { get; }
+
+        /// <summary>
+        /// Create new instances of the <see cref="ProtoRpcAttribute"/> class.
+        /// </summary>
+        /// <param name="rpcType">The RPC method type.</param>
+        public ProtoRpcAttribute(ProtoRpcType rpcType)
+        {
+            RpcType = rpcType;
+        }
+    }
+}
