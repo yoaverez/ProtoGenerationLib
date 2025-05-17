9aad0de7659a22007fc2283d718fd6173e118aa4
yoaverez <yoav.erez.01@gmail.com>
2025-05-17 09:27:22 +0300

    Create new attribute for optional data members

---
+++ src/ProtoGenerator/Attributes/OptionalDataMemberAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProtoGenerator.Attributes
+{
+    /// <summary>
+    /// Attribute for marking data members as optional members in the proto message.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter)]
+    public class OptionalDataMemberAttribute : Attribute
+    {
+    }
+}
