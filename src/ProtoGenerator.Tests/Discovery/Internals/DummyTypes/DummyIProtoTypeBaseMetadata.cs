e14ed41c80c0896b39b0fbeaec17eece1f8df8c9
yoaverez <yoav.erez.01@gmail.com>
2025-05-31 13:30:08 +0300

    Add unit tests for the ProtoTypeMetadataDiscoverer

---
+++ src/ProtoGenerator.Tests/Discovery/Internals/DummyTypes/DummyIProtoTypeBaseMetadata.cs
@@ -0,0 +1,13 @@
+﻿using ProtoGenerator.Models.Abstracts.ProtoDefinitions;
+
+namespace ProtoGenerator.Tests.Discovery.Internals.DummyTypes
+{
+    internal class DummyIProtoTypeBaseMetadata : IProtoTypeBaseMetadata
+    {
+        public string? Name { get; set; }
+
+        public string? Package { get; set; }
+
+        public string? FilePath { get; set; }
+    }
+}
