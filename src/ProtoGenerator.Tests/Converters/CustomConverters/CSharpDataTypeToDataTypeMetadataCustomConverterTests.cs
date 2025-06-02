915387e6413a815b7c8091a4d62b3bd06eef7cdd
yoaverez <yoav.erez.01@gmail.com>
2025-06-02 11:13:36 +0300

    Implemented and tested the abstract custom converters

---
+++ src/ProtoGenerator.Tests/Converters/CustomConverters/CSharpDataTypeToDataTypeMetadataCustomConverterTests.cs
@@ -0,0 +1,166 @@
+﻿using HarmonyLib;
+using ProtoGenerator.Configurations.Abstracts;
+using ProtoGenerator.Configurations.Internals;
+using ProtoGenerator.Converters.CustomConverters;
+using ProtoGenerator.Extractors.Internals.TypesExtractors;
+using ProtoGenerator.Models.Abstracts.IntermediateRepresentations;
+using ProtoGenerator.Models.Internals.IntermediateRepresentations;
+using System.Reflection;
+
+namespace ProtoGenerator.Tests.Converters.CustomConverters
+{
+    [TestClass]
+    public class CSharpDataTypeToDataTypeMetadataCustomConverterTests
+    {
+        private class CustomConverter : CSharpDataTypeToDataTypeMetadataCustomConverter
+        {
+            public Func<Type, IProtoGenerationOptions, bool> CanHandleTypeIProtoGenerationOptions { get; set; }
+            public Func<Type, IProtoGenerationOptions, IDataTypeMetadata> BaseConvertTypeToIntermediateRepresentationTypeIProtoGenerationOptions { get; set; }
+
+            public CustomConverter()
+            {
+                CanHandleTypeIProtoGenerationOptions = (a, b) => default;
+                BaseConvertTypeToIntermediateRepresentationTypeIProtoGenerationOptions = (a, b) => default;
+            }
+
+            public override bool CanHandle(Type type, IProtoGenerationOptions generationOptions)
+            {
+                return CanHandleTypeIProtoGenerationOptions(type, generationOptions);
+            }
+
+            protected override IDataTypeMetadata BaseConvertTypeToIntermediateRepresentation(Type type, IProtoGenerationOptions generationOptions)
+            {
+                return BaseConvertTypeToIntermediateRepresentationTypeIProtoGenerationOptions(type, generationOptions);
+            }
+        }
+
+        private static IProtoGenerationOptions generationOptions;
+
+        private CustomConverter customConverter;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            generationOptions = new ProtoGenerationOptions();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            customConverter = new CustomConverter();
+        }
+
+        #region ConvertTypeToIntermediateRepresentation Tests
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void ConvertTypeToIntermediateRepresentation_CanNotHandleType_ThrowsArgumentException()
+        {
+            // Arrange
+            customConverter.CanHandleTypeIProtoGenerationOptions = (a, b) => false;
+            var type = typeof(int);
+
+            // Act
+            customConverter.ConvertTypeToIntermediateRepresentation(type, generationOptions);
+
+            // Assert
+            // Noting to do.
+            // The ExpectedException will do the assert.
+        }
+
+        [TestMethod]
+        public void ConvertTypeToIntermediateRepresentation_CanHandleType_ReturnSameMetadataAsBaseMethod()
+        {
+            // Arrange
+            customConverter.CanHandleTypeIProtoGenerationOptions = (a, b) => true;
+
+            var expectedMetadata = CreateDataTypeMetadata(new List<Type>
+            {
+                typeof(int), typeof(bool), typeof(string)
+            });
+            customConverter.BaseConvertTypeToIntermediateRepresentationTypeIProtoGenerationOptions = (a, b) =>
+            {
+                return expectedMetadata;
+            };
+            var type = typeof(int);
+
+            // Act
+            var actualMetadata = customConverter.ConvertTypeToIntermediateRepresentation(type, generationOptions);
+
+            // Assert
+            Assert.AreSame(expectedMetadata, actualMetadata);
+        }
+
+        #endregion ConvertTypeToIntermediateRepresentation Tests
+
+        #region ExtractUsedTypes Tests
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void ExtractUsedTypes_CanNotHandleType_ThrowsArgumentException()
+        {
+            // Arrange
+            customConverter.CanHandleTypeIProtoGenerationOptions = (a, b) => false;
+            var type = typeof(int);
+
+            // Act
+            customConverter.ExtractUsedTypes(type, generationOptions);
+
+            // Assert
+            // Noting to do.
+            // The ExpectedException will do the assert.
+        }
+
+        [TestMethod]
+        public void ExtractUsedTypes_CanHandleType_ReturnAllTheUsedTypes()
+        {
+            // Arrange
+            customConverter.CanHandleTypeIProtoGenerationOptions = (a, b) => true;
+
+            var typesInMetadata = new List<Type>
+            {
+                typeof(int), typeof(bool), typeof(string)
+            };
+            var expectedMetadata = CreateDataTypeMetadata(typesInMetadata);
+            customConverter.BaseConvertTypeToIntermediateRepresentationTypeIProtoGenerationOptions = (a, b) =>
+            {
+                return expectedMetadata;
+            };
+
+            var type = typeof(int);
+            var expectedUsedTypes = typesInMetadata.Append(typeof(void)).ToList();
+
+            var harmony = new Harmony(nameof(ExtractUsedTypes_CanHandleType_ReturnAllTheUsedTypes));
+            var originMethod = typeof(FieldsTypesExtractor).GetMethod(nameof(FieldsTypesExtractor.ExtractUsedTypesFromFields));
+            static bool Prefix(ref IEnumerable<Type> __result, IEnumerable<Type> fieldTypes, IProtoGenerationOptions generationOptions)
+            {
+                __result = fieldTypes.Append(typeof(void)).ToList();
+                return false;
+            }
+            harmony.Patch(originMethod, new HarmonyMethod(Prefix));
+
+            // Act
+            var actualUsedTypes = customConverter.ExtractUsedTypes(type, generationOptions);
+
+            // Clean harmony
+            harmony.UnpatchAll();
+
+            // Assert
+            CollectionAssert.AreEquivalent(expectedUsedTypes, actualUsedTypes.ToList());
+        }
+
+        #endregion ExtractUsedTypes Tests
+
+        #region Auxiliary Functions
+
+        private static DataTypeMetadata CreateDataTypeMetadata(IEnumerable<Type> fieldTypes)
+        {
+            return new DataTypeMetadata
+            {
+                Fields = fieldTypes.Select(x => new FieldMetadata() { Type = x }).Cast<IFieldMetadata>().ToList(),
+            };
+        }
+
+        #endregion Auxiliary Functions
+    }
+}
---
+++ src/ProtoGenerator/Converters/CustomConverters/CSharpContractTypeToContractTypeMetadataCustomConverter.cs
@@ -0,0 +1,69 @@
+﻿using ProtoGenerator.Configurations.Abstracts;
+using ProtoGenerator.Converters.Abstracts;
+using ProtoGenerator.Models.Abstracts.IntermediateRepresentations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProtoGenerator.Converters.CustomConverters
+{
+    /// <summary>
+    /// Abstract custom converter for contract types.
+    /// </summary>
+    /// <remarks>
+    /// Implementing this will make the customization more safe
+    /// and easy for the user, however this allows less flexibility
+    /// then just implementing the <see cref="ICSharpToIntermediateCustomConverter{TIntermediate}"/>.
+    /// </remarks>
+    public abstract class CSharpContractTypeToContractTypeMetadataCustomConverter : ICSharpToIntermediateCustomConverter<IContractTypeMetadata>
+    {
+        /// <inheritdoc/>
+        /// <exception cref="Exception">
+        /// Thrown when <see cref="BaseConvertTypeToIntermediateRepresentation(Type, IProtoGenerationOptions)"/>
+        /// returns a metadata with at least one method that have more than one parameter.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the given <paramref name="type"/> can not be handled by this custom converter.
+        /// </exception>
+        public IContractTypeMetadata ConvertTypeToIntermediateRepresentation(Type type, IProtoGenerationOptions generationOptions)
+        {
+            if (!CanHandle(type, generationOptions))
+                throw new ArgumentException($"The given {nameof(type)}: {type.Name} can not be handled by this custom converter.");
+
+            var metadata = BaseConvertTypeToIntermediateRepresentation(type, generationOptions);
+            foreach (var method in metadata.Methods)
+            {
+                if (method.Parameters.Count() > 1)
+                    throw new Exception($"The metadata you created contains a method named {method.MethodInfo.Name} that have more than one parameter.");
+            }
+            return metadata;
+        }
+
+        /// <inheritdoc/>
+        /// <inheritdoc cref="ConvertTypeToIntermediateRepresentation(Type, IProtoGenerationOptions)" path="/exception"/>
+        public virtual IEnumerable<Type> ExtractUsedTypes(Type type, IProtoGenerationOptions generationOptions)
+        {
+            var metadata = ConvertTypeToIntermediateRepresentation(type, generationOptions);
+            var usedTypes = new HashSet<Type>();
+            foreach (var method in metadata.Methods)
+            {
+                usedTypes.Add(method.ReturnType);
+                if (method.Parameters.Count() == 0)
+                    usedTypes.Add(typeof(void));
+                else
+                    // If there are more than on parameter
+                    // the ConvertTypeToIntermediateRepresentation
+                    // would thrown an exception.
+                    usedTypes.Add(method.Parameters.Single().Type);
+            }
+            return usedTypes;
+        }
+
+        /// <inheritdoc/>
+        public abstract bool CanHandle(Type type, IProtoGenerationOptions generationOptions);
+
+        /// <inheritdoc cref="ICSharpToIntermediateConverter{TIntermediate}.ConvertTypeToIntermediateRepresentation(Type, IProtoGenerationOptions)"/>
+        /// <remarks><b>Note: All the methods in the returned metadata MUST have one parameter or less.</b></remarks>
+        protected abstract IContractTypeMetadata BaseConvertTypeToIntermediateRepresentation(Type type, IProtoGenerationOptions generationOptions);
+    }
+}
