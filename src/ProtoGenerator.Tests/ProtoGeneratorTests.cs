4f4df94d785c5296f8befdcd7caebe0b4078cd8c
yoaverez <yoav.erez.01@gmail.com>
2025-06-14 16:56:05 +0300

    Implemented and add Tests for the ProtoGenerator

---
+++ src/ProtoGenerator.Tests/ProtoGeneratorTests.cs
@@ -0,0 +1,225 @@
+﻿using Moq;
+using ProtoGenerator.Configurations.Abstracts;
+using ProtoGenerator.Configurations.Internals;
+using ProtoGenerator.Converters.Abstracts;
+using ProtoGenerator.Discovery.Abstracts;
+using ProtoGenerator.Extractors.Abstracts;
+using ProtoGenerator.Models.Abstracts.ProtoDefinitions;
+using ProtoGenerator.Models.Internals.ProtoDefinitions;
+using ProtoGenerator.ProvidersAndRegistries.Abstracts.Providers;
+using ProtoGenerator.Utilities.CollectionUtilities;
+
+namespace ProtoGenerator.Tests
+{
+    [TestClass]
+    public class ProtoGeneratorTests
+    {
+        private Mock<IProtoTypesExtractor> mockProtoTypesExtractor;
+
+        private Mock<IProtoTypeMetadataDiscoverer> mockProtoTypeMetadataDiscoverer;
+
+        private Mock<ICSharpToProtoTypesConverter> mockCSharpToProtoTypesConverter;
+
+        private HashSet<Type> wellKnownTypes;
+
+        private ProtoGenerator protoGenerator;
+
+        private ProtoGenerationOptions generationOptions;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mockProtoTypesExtractor = new Mock<IProtoTypesExtractor>();
+            mockProtoTypeMetadataDiscoverer = new Mock<IProtoTypeMetadataDiscoverer>();
+            mockCSharpToProtoTypesConverter = new Mock<ICSharpToProtoTypesConverter>();
+            wellKnownTypes = new HashSet<Type>();
+
+            var mockIProvider = new Mock<IProvider>();
+
+            protoGenerator = new ProtoGenerator(mockIProvider.Object,
+                                                mockProtoTypesExtractor.Object,
+                                                mockProtoTypeMetadataDiscoverer.Object,
+                                                mockCSharpToProtoTypesConverter.Object,
+                                                wellKnownTypes);
+            generationOptions = new ProtoGenerationOptions();
+        }
+
+        [TestMethod]
+        public void GenerateProtos_SingleType_FlowIsCorrect()
+        {
+            // Arrange
+            var actualFlow = new List<string>();
+            var expectedFlow = new List<string>
+            {
+                nameof(IProtoTypesExtractor.ExtractProtoTypes),
+                nameof(IProtoTypeMetadataDiscoverer.DiscoverProtosMetadata),
+                nameof(ICSharpToProtoTypesConverter.Convert),
+            };
+
+            wellKnownTypes.AddRange(new Type[] { typeof(float), typeof(double) });
+            IReadOnlyDictionary<Type,Type> originToNewTypeMapping = new Dictionary<Type, Type>
+            {
+                [typeof(int)] = typeof(uint),
+            };
+
+            var extractorResult = new Type[] { typeof(object), typeof(double), typeof(string) };
+            IEnumerable<Type> extractorGivenTypes = null;
+            IProtoGenerationOptions extractorGivenOptions = null;
+            mockProtoTypesExtractor.Setup(x => x.ExtractProtoTypes(It.IsAny<IEnumerable<Type>>(), It.IsAny<IProtoGenerationOptions>(), out originToNewTypeMapping))
+                                   .Callback(() => actualFlow.Add(nameof(IProtoTypesExtractor.ExtractProtoTypes)))
+                                   .Returns<IEnumerable<Type>, IProtoGenerationOptions, IReadOnlyDictionary<Type, Type>>((types, options, x) =>
+                                   {
+                                       extractorGivenTypes = types;
+                                       extractorGivenOptions = options;
+                                       return extractorResult;
+                                   });
+
+            var discovererResult = new Dictionary<Type, IProtoTypeMetadata>
+            {
+                [typeof(uint)] = new ProtoTypeMetadata("a", "pac", "pac.a", "path"),
+            };
+            IEnumerable<Type> discovererGivenTypes = null;
+            IProtoGenerationOptions discovererGivenOptions = null;
+            mockProtoTypeMetadataDiscoverer.Setup(x => x.DiscoverProtosMetadata(It.IsAny<IEnumerable<Type>>(), It.IsAny<IProtoGenerationOptions>()))
+                                           .Callback(() => actualFlow.Add(nameof(IProtoTypeMetadataDiscoverer.DiscoverProtosMetadata)))
+                                           .Returns<IEnumerable<Type>, IProtoGenerationOptions>((types, options) =>
+                                           {
+                                               discovererGivenTypes = types;
+                                               discovererGivenOptions = options;
+                                               return discovererResult;
+                                           });
+
+            var typesToMetadataAfterAddingOriginTypes = new Dictionary<Type, IProtoTypeMetadata>
+            {
+                [typeof(uint)] = new ProtoTypeMetadata("a", "pac", "pac.a", "path"),
+                [typeof(int)] = new ProtoTypeMetadata("a", "pac", "pac.a", "path"),
+            };
+
+            var neededProtoTypesWithoutWellKnownTypes = new Type[] { typeof(object), typeof(string) };
+
+            var converterResult = new Dictionary<string, IProtoDefinition>
+            {
+                ["path"] = new ProtoDefinition(),
+            };
+            IEnumerable<Type> converterGivenTypes = null;
+            IReadOnlyDictionary<Type, IProtoTypeMetadata> converterGivenMetadatas = null;
+            IProtoGenerationOptions converterGivenOptions = null;
+            mockCSharpToProtoTypesConverter.Setup(x => x.Convert(It.IsAny<IEnumerable<Type>>(), It.IsAny<IReadOnlyDictionary<Type, IProtoTypeMetadata>>(), It.IsAny<IProtoGenerationOptions>()))
+                                           .Callback(() => actualFlow.Add(nameof(ICSharpToProtoTypesConverter.Convert)))
+                                           .Returns<IEnumerable<Type>, IReadOnlyDictionary<Type, IProtoTypeMetadata>, IProtoGenerationOptions>((types, metadatas, options) =>
+                                           {
+                                               converterGivenTypes = types;
+                                               converterGivenMetadatas = metadatas;
+                                               converterGivenOptions = options;
+                                               return converterResult;
+                                           });
+
+            var testedType = typeof(byte);
+
+            // Act
+            var actualDefinitions = protoGenerator.GenerateProtos(testedType, generationOptions);
+
+            // Assert
+            CollectionAssert.AreEqual(new Type[] { testedType }, extractorGivenTypes.ToArray());
+            Assert.AreSame(generationOptions, extractorGivenOptions);
+
+            CollectionAssert.AreEqual(extractorResult, discovererGivenTypes.ToArray());
+            Assert.AreSame(generationOptions, discovererGivenOptions);
+
+            CollectionAssert.AreEqual(neededProtoTypesWithoutWellKnownTypes, converterGivenTypes.ToArray());
+            CollectionAssert.AreEquivalent(typesToMetadataAfterAddingOriginTypes, converterGivenMetadatas.ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
+            Assert.AreSame(generationOptions, extractorGivenOptions);
+
+            CollectionAssert.AreEqual(expectedFlow, actualFlow);
+        }
+
+        [TestMethod]
+        public void GenerateProtos_MultipleTypes_FlowIsCorrect()
+        {
+            // Arrange
+            var actualFlow = new List<string>();
+            var expectedFlow = new List<string>
+            {
+                nameof(IProtoTypesExtractor.ExtractProtoTypes),
+                nameof(IProtoTypeMetadataDiscoverer.DiscoverProtosMetadata),
+                nameof(ICSharpToProtoTypesConverter.Convert),
+            };
+
+            wellKnownTypes.AddRange(new Type[] { typeof(float), typeof(double) });
+            IReadOnlyDictionary<Type, Type> originToNewTypeMapping = new Dictionary<Type, Type>
+            {
+                [typeof(int)] = typeof(uint),
+            };
+
+            var extractorResult = new Type[] { typeof(object), typeof(double), typeof(string) };
+            IEnumerable<Type> extractorGivenTypes = null;
+            IProtoGenerationOptions extractorGivenOptions = null;
+            mockProtoTypesExtractor.Setup(x => x.ExtractProtoTypes(It.IsAny<IEnumerable<Type>>(), It.IsAny<IProtoGenerationOptions>(), out originToNewTypeMapping))
+                                   .Callback(() => actualFlow.Add(nameof(IProtoTypesExtractor.ExtractProtoTypes)))
+                                   .Returns<IEnumerable<Type>, IProtoGenerationOptions, IReadOnlyDictionary<Type, Type>>((types, options, x) =>
+                                   {
+                                       extractorGivenTypes = types;
+                                       extractorGivenOptions = options;
+                                       return extractorResult;
+                                   });
+
+            var discovererResult = new Dictionary<Type, IProtoTypeMetadata>
+            {
+                [typeof(uint)] = new ProtoTypeMetadata("a", "pac", "pac.a", "path"),
+            };
+            IEnumerable<Type> discovererGivenTypes = null;
+            IProtoGenerationOptions discovererGivenOptions = null;
+            mockProtoTypeMetadataDiscoverer.Setup(x => x.DiscoverProtosMetadata(It.IsAny<IEnumerable<Type>>(), It.IsAny<IProtoGenerationOptions>()))
+                                           .Callback(() => actualFlow.Add(nameof(IProtoTypeMetadataDiscoverer.DiscoverProtosMetadata)))
+                                           .Returns<IEnumerable<Type>, IProtoGenerationOptions>((types, options) =>
+                                           {
+                                               discovererGivenTypes = types;
+                                               discovererGivenOptions = options;
+                                               return discovererResult;
+                                           });
+
+            var typesToMetadataAfterAddingOriginTypes = new Dictionary<Type, IProtoTypeMetadata>
+            {
+                [typeof(uint)] = new ProtoTypeMetadata("a", "pac", "pac.a", "path"),
+                [typeof(int)] = new ProtoTypeMetadata("a", "pac", "pac.a", "path"),
+            };
+
+            var neededProtoTypesWithoutWellKnownTypes = new Type[] { typeof(object), typeof(string) };
+
+            var converterResult = new Dictionary<string, IProtoDefinition>
+            {
+                ["path"] = new ProtoDefinition(),
+            };
+            IEnumerable<Type> converterGivenTypes = null;
+            IReadOnlyDictionary<Type, IProtoTypeMetadata> converterGivenMetadatas = null;
+            IProtoGenerationOptions converterGivenOptions = null;
+            mockCSharpToProtoTypesConverter.Setup(x => x.Convert(It.IsAny<IEnumerable<Type>>(), It.IsAny<IReadOnlyDictionary<Type, IProtoTypeMetadata>>(), It.IsAny<IProtoGenerationOptions>()))
+                                           .Callback(() => actualFlow.Add(nameof(ICSharpToProtoTypesConverter.Convert)))
+                                           .Returns<IEnumerable<Type>, IReadOnlyDictionary<Type, IProtoTypeMetadata>, IProtoGenerationOptions>((types, metadatas, options) =>
+                                           {
+                                               converterGivenTypes = types;
+                                               converterGivenMetadatas = metadatas;
+                                               converterGivenOptions = options;
+                                               return converterResult;
+                                           });
+
+            var testedTypes = new Type[] { typeof(byte), typeof(sbyte) };
+
+            // Act
+            var actualDefinitions = protoGenerator.GenerateProtos(testedTypes, generationOptions);
+
+            // Assert
+            CollectionAssert.AreEqual(testedTypes, extractorGivenTypes.ToArray());
+            Assert.AreSame(generationOptions, extractorGivenOptions);
+
+            CollectionAssert.AreEqual(extractorResult, discovererGivenTypes.ToArray());
+            Assert.AreSame(generationOptions, discovererGivenOptions);
+
+            CollectionAssert.AreEqual(neededProtoTypesWithoutWellKnownTypes, converterGivenTypes.ToArray());
+            CollectionAssert.AreEquivalent(typesToMetadataAfterAddingOriginTypes, converterGivenMetadatas.ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
+            Assert.AreSame(generationOptions, extractorGivenOptions);
+
+            CollectionAssert.AreEqual(expectedFlow, actualFlow);
+        }
+    }
+}
---
+++ src/ProtoGenerator/ProtoGenerator.cs
@@ -0,0 +1,121 @@
+﻿using ProtoGenerator.Configurations.Abstracts;
+using ProtoGenerator.Constants;
+using ProtoGenerator.Converters.Abstracts;
+using ProtoGenerator.Converters.Internals;
+using ProtoGenerator.Discovery.Abstracts;
+using ProtoGenerator.Discovery.Internals;
+using ProtoGenerator.Extractors.Abstracts;
+using ProtoGenerator.Extractors.Internals;
+using ProtoGenerator.Models.Abstracts.ProtoDefinitions;
+using ProtoGenerator.ProvidersAndRegistries.Abstracts.Providers;
+using ProtoGenerator.Utilities.CollectionUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProtoGenerator
+{
+    /// <summary>
+    /// Generator of <see cref="IProtoDefinition"/> from c# types.
+    /// </summary>
+    public class ProtoGenerator
+    {
+        /// <inheritdoc cref="IProtoTypesExtractor"/>
+        private IProtoTypesExtractor protoTypesExtractor;
+
+        /// <inheritdoc cref="IProtoTypeMetadataDiscoverer"/>
+        private IProtoTypeMetadataDiscoverer protoTypeMetadataDiscoverer;
+
+        /// <inheritdoc cref="ICSharpToProtoTypesConverter"/>
+        private ICSharpToProtoTypesConverter csharpToProtoTypesConverter;
+
+        /// <summary>
+        /// A set of all the well known types in protobuf language.
+        /// </summary>
+        private ISet<Type> wellKnownTypes;
+
+        /// <summary>
+        /// Create new instance of the <see cref="ProtoGenerator"/> class.
+        /// </summary>
+        /// <param name="componentsProvider">A provider of all the proto generator customizations.</param>
+        /// <param name="protoTypesExtractor"><inheritdoc cref="protoTypesExtractor" path="/node()"/></param>
+        /// <param name="protoTypeMetadataDiscoverer"><inheritdoc cref="protoTypeMetadataDiscoverer" path="/node()"/></param>
+        /// <param name="csharpToProtoTypesConverter"><inheritdoc cref="csharpToProtoTypesConverter" path="/node()"/></param>
+        /// <param name="wellKnownTypes"><inheritdoc cref="wellKnownTypes" path="/node()"/></param>
+        public ProtoGenerator(IProvider componentsProvider,
+                              IProtoTypesExtractor? protoTypesExtractor = null,
+                              IProtoTypeMetadataDiscoverer? protoTypeMetadataDiscoverer = null,
+                              ICSharpToProtoTypesConverter? csharpToProtoTypesConverter = null,
+                              ISet<Type>? wellKnownTypes = null)
+        {
+            this.protoTypesExtractor = protoTypesExtractor ?? new ProtoTypesExtractor(componentsProvider);
+            this.protoTypeMetadataDiscoverer = protoTypeMetadataDiscoverer ?? new ProtoTypeMetadataDiscoverer(componentsProvider);
+            this.csharpToProtoTypesConverter = csharpToProtoTypesConverter ?? new CSharpToProtoConverter(componentsProvider);
+            this.wellKnownTypes = wellKnownTypes ?? new HashSet<Type>(WellKnownTypesConstants.WellKnownTypes.Keys);
+        }
+
+        /// <summary>
+        /// Generate <see cref="IProtoDefinition"/> for the given <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">The type to generate the protos from.</param>
+        /// <param name="generationOptions">The generation options.</param>
+        /// <returns>A mapping between file relative path to it proto file definition.</returns>
+        public IDictionary<string, IProtoDefinition> GenerateProtos(Type type, IProtoGenerationOptions generationOptions)
+        {
+            return GenerateProtos(new Type[] { type }, generationOptions);
+        }
+
+        /// <summary>
+        /// Generate <see cref="IProtoDefinition"/> for the given <paramref name="types"/>.
+        /// </summary>
+        /// <param name="types">The types to generate the protos from.</param>
+        /// <param name="generationOptions">The generation options.</param>
+        /// <returns>A mapping between file relative path to it proto file definition.</returns>
+        public IDictionary<string, IProtoDefinition> GenerateProtos(IEnumerable<Type> types, IProtoGenerationOptions generationOptions)
+        {
+            // Extract all the used c# types that are needed for the
+            // proto generation.
+            var usedTypes = protoTypesExtractor.ExtractProtoTypes(types, generationOptions, out var originTypeToNewTypeMapping);
+
+            // Discover all the above used types proto metadata.
+            var typesToMetadatas = protoTypeMetadataDiscoverer.DiscoverProtosMetadata(usedTypes, generationOptions);
+
+            // Add the meta data for c# types that were replaced
+            // with a new type in order to become proto type.
+            AddOriginTypesMetadata(typesToMetadatas, originTypeToNewTypeMapping);
+
+            // Remove the well known types from the used types
+            // in order to get all the types that needs to be
+            // created in protobuf.
+            var neededProtoTypes = usedTypes.Where(type => !wellKnownTypes.Contains(type)).ToArray();
+
+            // Convert all the needed proto types to proto file definitions.
+            var fileRelativePathToProtoDefinitions = csharpToProtoTypesConverter.Convert(neededProtoTypes,
+                                                                                         typesToMetadatas,
+                                                                                         generationOptions);
+
+            return fileRelativePathToProtoDefinitions;
+        }
+
+        /// <summary>
+        /// Add the metadata of types that were replaced
+        /// to the given <paramref name="protoTypesMetadatas"/>.
+        /// </summary>
+        /// <param name="protoTypesMetadatas">The mapping from type to its proto metadata.</param>
+        /// <param name="originTypeToNewTypeMapping">The mapping from replaced types to their replacers.</param>
+        /// <remarks>
+        /// The metadata of types that were replaced is the
+        /// metadata of the types that replace them.
+        /// </remarks>
+        private static void AddOriginTypesMetadata(IDictionary<Type, IProtoTypeMetadata> protoTypesMetadatas,
+                                                   IReadOnlyDictionary<Type, Type> originTypeToNewTypeMapping)
+        {
+            foreach (var item in originTypeToNewTypeMapping)
+            {
+                var originType = item.Key;
+                var newType = item.Value;
+                protoTypesMetadatas[originType] = protoTypesMetadatas[newType];
+            }
+        }
+    }
+}
