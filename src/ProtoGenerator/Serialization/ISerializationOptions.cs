b13408da2f1ab75c55aefda062010dba2f9dc93b
yoaverez <yoav.erez.01@gmail.com>
2025-06-16 16:07:57 +0300

    Add serialization of proto definitions

---
+++ src/ProtoGenerator/Serialization/ISerializationOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace ProtoGenerator.Serialization
+{
+    /// <summary>
+    /// A contract representing the proto generation serialization options.
+    /// </summary>
+    public interface ISerializationOptions
+    {
+        /// <summary>
+        /// The indentation size in spaces.
+        /// e.g. IndentSize = x means that each tab/indentation is x spaces.
+        /// </summary>
+        uint IndentSize { get; }
+    }
+}
