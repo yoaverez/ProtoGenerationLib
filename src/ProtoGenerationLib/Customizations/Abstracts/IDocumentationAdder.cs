e137a334057d634396969f34781d7af61bed436a
yoaverez <yoav.erez.01@gmail.com>
2025-06-28 19:27:45 +0300

    Add documentation customization

---
+++ src/ProtoGenerationLib/Customizations/Abstracts/IDocumentationAdder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ProtoGenerationLib.Customizations.Abstracts
+{
+    /// <summary>
+    /// A contract for adding documentation to types and members.
+    /// </summary>
+    public interface IDocumentationAdder
+    {
+        /// <summary>
+        /// Associate the given <paramref name="documentation"/>
+        /// with the given <typeparamref name="TType"/>.
+        /// </summary>
+        /// <typeparam name="TType">The type to associate the <paramref name="documentation"/> with.</typeparam>
+        /// <param name="documentation">The documentation to add to the given <typeparamref name="TType"/>.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when there is already a documentation that is associated
+        /// with the given <typeparamref name="TType"/>.
+        /// </exception>
+        void AddDocumentation<TType>(string documentation);
+
+        /// <summary>
+        /// Associate the given <paramref name="documentation"/>
+        /// with the field with the given <paramref name="fieldName"/> that
+        /// is declared in the given <typeparamref name="TFieldDeclaringType"/>.
+        /// </summary>
+        /// <typeparam name="TFieldDeclaringType">The type that declares the requested field.</typeparam>
+        /// <param name="fieldName">The name of the field to associate the <paramref name="documentation"/> with.</param>
+        /// <param name="documentation">The documentation to add to the field.</param>
+        /// <remarks>
+        /// The method also works for properties.
+        /// </remarks>
+        /// <exception cref="ArgumentException">
+        /// Thrown when there is already a documentation that is associated
+        /// with the field with the given <paramref name="fieldName"/> that was declared
+        /// in the given <typeparamref name="TFieldDeclaringType"/>.
+        /// </exception>
+        void AddDocumentation<TFieldDeclaringType>(string fieldName, string documentation);
+
+        /// <summary>
+        /// Associate the given <paramref name="documentation"/>
+        /// with the method with the given <paramref name="methodName"/> that
+        /// is declared in the given <typeparamref name="TMethodDeclaringType"/>.
+        /// </summary>
+        /// <typeparam name="TMethodDeclaringType">The type that declares the requested method.</typeparam>
+        /// <param name="methodName">The name of the method to associate the <paramref name="documentation"/> with.</param>
+        /// <param name="numOfParameters">
+        /// The number of the parameters of the method.
+        /// Needed in order to associate the documentation the right method.
+        /// </param>
+        /// <param name="documentation">The documentation to add to the method.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when there is already a documentation that is associated
+        /// with the method with the given <paramref name="methodName"/> that was declared
+        /// in the given <typeparamref name="TMethodDeclaringType"/>.
+        /// </exception>
+        void AddDocumentation<TMethodDeclaringType>(string methodName, int numOfParameters, string documentation);
+
+        /// <summary>
+        /// Associate the given <paramref name="documentation"/>
+        /// with the given <paramref name="enumValue"/>.
+        /// </summary>
+        /// <typeparam name="TEnumType">The type of the enum whose value documentation is requested.</typeparam>
+        /// <param name="enumValue">The value of the enum.</param>
+        /// <param name="documentation">The documentation to add to the given <paramref name="enumValue"/>.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when there is already a documentation that is associated
+        /// with the given <paramref name="enumValue"/>.
+        /// </exception>
+        void AddDocumentation<TEnumType>(int enumValue, string documentation) where TEnumType : Enum;
+    }
+}
---
+++ src/ProtoGenerationLib/Customizations/Abstracts/IDocumentationProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ProtoGenerationLib.Customizations.Abstracts
+{
+    /// <summary>
+    /// A provider for types and members documentation.
+    /// </summary>
+    public interface IDocumentationProvider
+    {
+        /// <summary>
+        /// Try getting the documentation of the given <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">The type whose documentation is requested.</param>
+        /// <param name="documentation">The documentation if found.</param>
+        /// <returns>
+        /// <see langword="true"/> if the given type has documentation associated with
+        /// it otherwise <see langword="false"/>
+        /// </returns>
+        bool TryGetTypeDocumentation(Type type, out string documentation);
+
+        /// <summary>
+        /// Try getting the documentation of the field with the
+        /// given <paramref name="fieldName"/> that is declared inside the given
+        /// <paramref name="fieldDeclaringType"/>.
+        /// </summary>
+        /// <param name="fieldDeclaringType">The type that declared the field.</param>
+        /// <param name="fieldName">The name of the field.</param>
+        /// <param name="documentation">The documentation of the field if found.</param>
+        /// <returns>
+        /// <see langword="true"/> if the field with the given <paramref name="fieldName"/> has
+        /// documentation associated with it otherwise <see langword="false"/>
+        /// </returns>
+        bool TryGetFieldDocumentation(Type fieldDeclaringType, string fieldName, out string documentation);
+
+        /// <summary>
+        /// Try getting the documentation of the method with the
+        /// given <paramref name="methodName"/> that is declared inside the given
+        /// <paramref name="methodDeclaringType"/>.
+        /// </summary>
+        /// <param name="methodDeclaringType">The type that declared the method.</param>
+        /// <param name="methodName">The name of the method.</param>
+        /// <param name="methodNumOfParams">The number of method parameters.</param>
+        /// <param name="documentation">The method documentation if found.</param>
+        /// <returns>
+        /// <see langword="true"/> if the method with the given <paramref name="methodName"/> and
+        /// <paramref name="methodNumOfParams"/> has documentation associated with it
+        /// otherwise <see langword="false"/>
+        /// </returns>
+        bool TryGetMethodDocumentation(Type methodDeclaringType, string methodName, int methodNumOfParams, out string documentation);
+
+        /// <summary>
+        /// Try getting the documentation of the enum value with the
+        /// given <paramref name="enumValue"/> that is declared inside the given
+        /// <paramref name="enumType"/>.
+        /// </summary>
+        /// <param name="enumType">The enum types that declares the enum value.</param>
+        /// <param name="enumValue">The enum value.</param>
+        /// <param name="documentation">The documentation of the enum value if found.</param>
+        /// <returns>
+        /// <see langword="true"/> if the enum value with the given <paramref name="enumValue"/>
+        /// has documentation associated with it otherwise <see langword="false"/>
+        /// </returns>
+        bool TryGetEnumValueDocumentation(Type enumType, int enumValue, out string documentation);
+    }
+}
